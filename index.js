a37b6656d184467e9e2fcf9de02da135b5ce9a3b
muhammadBobur0 <muhammadboburbek@gmail.com>
2023-09-13 12:53:17 +0500

    added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const express = require('express')
 const { readFileSync, writeFileSync } = require('fs');
+const app = express()
 const { resolve } = require('path');
 const cors = require('cors');
+app.use(cors())
 const PORT = process.env.PORT || 3000;
 const TelegramBot = require('node-telegram-bot-api');
 
